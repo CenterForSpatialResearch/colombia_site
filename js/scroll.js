df37109b1560aeb7e85a10681fb9ff849a10900b
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2017-02-06 18:35:05 -0500

    update

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,12 @@
-$('a[href^="#"]').on('click', function(event) {
+$(document).ready(function() {
+console.log('loaded...');
+  $('a').on('click', function(event) {
     var target = $(this.getAttribute('href'));
-    console.log(target);
     if( target.length ) {
         event.preventDefault();
         $('html, body').stop().animate({
             scrollTop: target.offset().top
-        }, 1000);
+        }, 500);
     }
+  });
 });
