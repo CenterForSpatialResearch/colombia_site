218699dbc67d2818eb61298d850aa003eb84745f
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2017-02-06 16:51:10 -0500

    update

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,10 @@
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    console.log(target);
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
     }
-  });
 });
